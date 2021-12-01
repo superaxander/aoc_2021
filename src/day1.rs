4727239bef4f12db3ee448e07674d3e5b2db51e8
Alexander Stekelenburg <anoniem.axander@gmail.com>
2021-12-01 07:09:38 +0100

    Initial commit (day1 part 1 and part2)

---
+++ src/day1.rs
@@ -0,0 +1,44 @@
+use std::collections::HashSet;
+use std::io;
+
+use crate::common;
+
+pub fn main() -> io::Result<(i32, i32)> {
+    let mut sums = Vec::new();
+
+    let lines = common::read_lines("inputs/1.txt")?;
+    let mut solution_a = 0;
+    let mut solution_b = 0;
+    let mut last_number = i32::MAX;
+    for line in lines {
+        if let Ok(num) = line?.trim().parse::<i32>() {
+            if num > last_number {
+                solution_a += 1;
+                debug!("{} > {}, {}", num, last_number, solution_a)
+            }
+            let len = sums.len();
+            if len > 2 {
+                sums[len - 2] += num;
+                sums[len - 1] += num;
+                sums.push(num);
+            } else if len > 1 {
+                sums[len - 1] += num;
+                sums.push(num);
+            } else {
+                sums.push(num)
+            }
+            
+            last_number = num;
+        }
+    }
+    let mut last_number = i32::MAX;
+    for sum in sums {
+        if sum > last_number {
+            debug!("{} > {}", sum, last_number);
+            solution_b += 1
+        }
+        last_number = sum;
+    }
+
+    return Ok((solution_a, solution_b));
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+
+use common::{Day, Runnable};
+use std::time::Instant;
+
+mod common;
+mod day1;
+// mod day10;
+// mod day11;
+// mod day12;
+// mod day13;
+// mod day14;
+// mod day15;
+// mod day16;
+// mod day2;
+// mod day3;
+// mod day4;
+// mod day5;
+// mod day6;
+// mod day7;
+// mod day8;
+// mod day9;
+
+fn main() {
+    pretty_env_logger::init();
+    let now = Instant::now();
+    Day::Combined(day1::main).run("day 1");
+    // Day::Separated(day2::main).run("day 2");
+    // Day::SeparatedLong(day3::main).run("day 3");
+    // Day::Separated(day4::main).run("day 4");
+    // Day::Separated(day5::main).run("day 5");
+    // Day::SeparatedUsize(day6::main).run("day 6");
+    // Day::SeparatedUsize(day7::main).run("day 7");
+    // Day::Separated(day8::main).run("day 8");
+    // Day::CombinedUsize(day9::main).run("day 9");
+    // Day::SeparatedUsize(day10::main).run("day 10");
+    // Day::SeparatedUsize(day11::main).run("day 11");
+    // Day::Separated(day12::main).run("day 12");
+    // Day::SeparatedULong(day13::main).run("day 13");
+    // Day::SeparatedULong(day14::main).run("day 14");
+    // Day::SeparatedUsize(day15::main).run("day 15");
+    // Day::CombinedUsize(day16::main).run("day 16");
+    info!("All days together took {:#?}", now.elapsed());
+}
