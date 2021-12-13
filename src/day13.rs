ae3220893912d9c37acaecf5e7c592e4d24acc09
Alexander Stekelenburg <anoniem.axander@gmail.com>
2021-12-13 08:37:35 +0100

    A different approach for day 13

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::io;
 
 use crate::common;
@@ -5,13 +6,14 @@ use crate::common;
 pub fn main() -> io::Result<(usize, usize)> {
     let lines = common::read_lines("inputs/13.txt")?;
 
-    let mut temp: Vec<usize> = Vec::new();
-    let mut folds: Vec<(bool, usize)> = Vec::new();
+    let mut points: HashSet<(usize, usize)> = HashSet::new();
+    let mut cnt = 0;
 
     let mut size_x = 0;
     let mut size_y = 0;
 
     let mut points_parsed = false;
+    let mut is_first = true;
     for line in lines {
         let line = line?;
         let line = line.trim();
@@ -22,64 +24,33 @@ pub fn main() -> io::Result<(usize, usize)> {
                 let split: Vec<&str> = line.split(',').collect();
                 let x = split[0].parse().unwrap();
                 let y = split[1].parse().unwrap();
-                size_x = size_x.max(x);
-                size_y = size_y.max(y);
-                temp.push(x);
-                temp.push(y);
+                points.insert((x, y));
             }
         } else {
             let split: Vec<&str> = line[11..].split('=').collect();
+            let fold: usize = split[1].parse().unwrap();
             match split[0] {
-                "x" => folds.push((false, split[1].parse().unwrap())),
-                "y" => folds.push((true, split[1].parse().unwrap())),
+                "x" => {
+                    points = points.iter().map(|(x, y)| if *x > fold { (fold * 2 - x, *y) } else { (*x, *y) }).collect();
+                    size_x = fold
+                }
+                "y" => {
+                    points = points.iter().map(|(x, y)| if *y > fold { (*x, fold * 2 - y) } else { (*x, *y) }).collect();
+                    size_y = fold
+                },
                 _ => debug!("Invalid axis: {}", split[0])
             }
+            if is_first {
+                cnt = points.len();
+                is_first = false;
+            }
         }
     }
-    size_x += 1;
-    size_y += 1;
 
-    let mut map = vec![false; size_x * size_y];
-
-    for i in (0..temp.len()).step_by(2) {
-        map[temp[i] + temp[i + 1] * size_x] = true;
-    }
-
-    let mut adjusted_size_x = size_x;
-    let mut cnt = 0;
-    let mut is_first = true;
-    for (is_y, fold) in folds {
-        if is_y {
-            for y in 0..fold {
-                for x in 0..adjusted_size_x {
-                    map[x + y * size_x] |= map[x + (size_y - y - 1) * size_x];
-                }
-            }
-            size_y = fold
-        } else {
-            for y in 0..size_y {
-                for x in 0..fold {
-                    map[x + y * size_x] |= map[adjusted_size_x - x - 1 + y * size_x];
-                }
-            }
-            adjusted_size_x = fold
-        }
-
-        if is_first {
-            for y in 0..size_y {
-                for x in 0..adjusted_size_x {
-                    if map[x + y * size_x] {
-                        cnt += 1;
-                    }
-                }
-            }
-            is_first = false;
-        }
-    }
     let mut string = String::new();
     for y in 0..size_y {
-        for x in 0..adjusted_size_x {
-            if map[x + y * size_x] {
+        for x in 0..size_x {
+            if points.contains(&(x, y)) {
                 string.push('#')
             } else {
                 string.push('-');
